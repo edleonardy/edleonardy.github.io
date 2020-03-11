e53c57fcfa2d7eb7074a5f9d979d02cdb26f8a51
Ed Leonardy <edwardleonardy@icloud.com>
2020-03-11 16:36:58 +0800

    Redesigned dashboard

--- dash/time.js
+++ dash/time.js
@@ -5,34 +5,68 @@ $(document).ready(function()
 
 function ShowTime()
 {
-    var d = new Date($.now());
-    document.getElementById('time').innerHTML = d.toLocaleTimeString("en-GB");
-    document.title = d.toLocaleTimeString("en-GB");
+    var date = new Date($.now());
+    var time = String(date.getHours()).padStart(2, 0) + ":" + String(date.getMinutes()).padStart(2, 0)
+    document.getElementById('time').innerHTML = time;
+    document.title = time;
 
-    var h = d.getHours()
-    var g = ""
-    if (h < 6)
+    var hour = date.getHours()
+    var greetingText = ""
+
+    if (hour < 6)
     {
-        g = "..it's late just go to bed";
-        imgSrc = "late.jpg";
+        greetingText = "..it's late just go to bed";
     }
-    else if (h < 12)
+    else if (hour < 12)
     {
-        g = "zou san.";
-        imgSrc = "morning.jpg";
+        greetingText = "zou san.";
     }
-    else if (h < 18)
+    else if (hour < 18)
     {
-        g = "buenas tardes.";
-        imgSrc = "afternoon.jpg";
+        greetingText = "buenas tardes.";
     }
     else
     {
-        g = "malam.";
-        imgSrc = "evening.jpg";
+        greetingText = "malam.";
     }
 
-    document.getElementById('greeting').innerHTML = g;
-    document.getElementById("bg").src=imgSrc;
+    weekText = "week " + weekOfYear(date) + " of " + weeksOfYear(date) + ".";
+    yearText = "year " + percentageYear(date) + " completed.";
+
+    greeting = date.toLocaleDateString("zh-TW") + weekText + "<br/>" + yearText
+    document.getElementById('greeting').innerHTML = greeting;
     window.setTimeout("ShowTime()", 1000);
 }
+
+function dayOfYear(date)
+{
+    var start = new Date(date.getFullYear(), 0, 0);
+    var difference = (date - start) + ((start.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000);
+    var oneDay = 1000 * 60 * 60 * 24;
+    return Math.floor(difference / oneDay);
+}
+
+function daysOfYear(date)
+{
+    var end = new Date(date.getFullYear()+1, 0, 0);
+    return dayOfYear(end);
+}
+
+function weekOfYear(date)
+{
+    var day = dayOfYear(date);
+    var start = new Date(date.getFullYear(), 0, 0);
+    return Math.floor((day + start.getDay()) / 7);
+}
+
+function weeksOfYear(date)
+{
+    var end = new Date(date.getFullYear()+1, 0, 0);
+    return weekOfYear(end);
+}
+
+function percentageYear(date)
+{
+    return (dayOfYear(date)/daysOfYear(date)*100).toFixed(2) + "%"
+}
+
